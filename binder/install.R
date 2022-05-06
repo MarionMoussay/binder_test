719459cb33be2c9cf28e9538cc4f0ba7b7403c0e
MarionMoussay <marion.moussay@hotmail.fr>
2022-05-06 10:04:39 +0200

    test git

--- binder/install.R
+++ binder/install.R
@@ -1,4 +1,6 @@
 install.packages("dplyr")
 install.packages("ggplot2")
 install.packages("SensoMineR")
-
+install.packages("devtools")
+library("devtools")
+devtools::install_github("cbail/textnets")
