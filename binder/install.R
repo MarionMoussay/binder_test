abd8c384c9110425429d6d5cc588c812a46e0a57
MarionMoussay <marion.moussay@hotmail.fr>
2022-05-06 13:59:45 +0200

    màj

--- binder/install.R
+++ binder/install.R
@@ -2,8 +2,8 @@ install.packages("dplyr")
 install.packages("ggplot2")
 install.packages("SensoMineR")
 
-install.packages("remotes")
-library(remotes)
-remotes::install_github("MaximeSaland/YesSiR", dependencies = TRUE)
+install.packages("githubinstall")
+
+
 
 
